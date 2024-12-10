6dd9c7bad32e0617ab76b061a3e0c072bde41885
Linininho <rodrigolino2007@gmail.com>
2024-12-10 09:42:05 +0000

    Add files via upload

---
+++ OOP-lino/classes_intro_obj.py
@@ -0,0 +1,10 @@
+from classes_intro_blueprints import Pessoa
+from classes_intro_blueprints import Carro
+ 
+
+pessoa = Pessoa("Rodrigo", 17)
+carro = Carro("Volkswagen", "mk7")
+ 
+ 
+print(pessoa.saudacao())
+print(carro.informacao_do_carro())
